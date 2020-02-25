ec04f65a6a9975a98c82f55b9f1b607798600cfc
ahamed.zoha@gmail.com <ahamed.zoha@gmail.com>
2020-02-25 18:06:00 +0600

    added a few dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App increment={3} />
+    </BrowserRouter>
 
 
-ReactDOM.render(<App increment={3}/>, document.getElementById('root'));
+    , document.getElementById('root'));
