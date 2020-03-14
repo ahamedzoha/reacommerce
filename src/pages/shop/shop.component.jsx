9d9d1fec69f56e6bbf0c535334a44e0504d2bff2
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-15 03:18:25 +0600

    Added page analytics listener to shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import SHOP_DATA from './shop.data.js'
-import { CollectionPreview } from '../../components/collection-preview/collection-preview.component'
-
+import React from "react"
+import SHOP_DATA from "./shop.data.js"
+import { CollectionPreview } from "../../components/collection-preview/collection-preview.component"
+import { init_google_analytics, log_page_view } from "../../utils/_analytics"
 
 export default class ShopPage extends React.Component {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            collections: SHOP_DATA
-        }
+    this.state = {
+      collections: SHOP_DATA
     }
+  }
 
+  componentDidMount() {
+    init_google_analytics()
+    log_page_view()
+  }
 
-    render() {
-        const { collections } = this.state
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-
-
-
-        )
-    }
+  render() {
+    const { collections } = this.state
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    )
+  }
 }
