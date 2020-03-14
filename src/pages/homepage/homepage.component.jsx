4473ab9224e2240b39357c32c6cd7997350238f5
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-14 23:00:54 +0600

    Added React Google Analytis as dependency + initialized react GA at homepage componenet

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './homepage.styles.scss'
+import React, { useEffect } from "react"
+import ReactGa from "react-ga"
+import "./homepage.styles.scss"
 //import { Link } from 'react-router-dom'
 
-import { Directory } from '../../components/directory/directory.component'
+import { Directory } from "../../components/directory/directory.component"
 
+export const HomePage = () => {
+  useEffect(() => {
+    ReactGa.initialize("UA-41008872-4")
 
+    // For Pageview
+    ReactGa.pageview("/")
+  }, [])
 
-export const HomePage = () => (
+  return (
     <div className="homepage">
-        <Directory />
+      <Directory />
     </div>
-)
+  )
+}
