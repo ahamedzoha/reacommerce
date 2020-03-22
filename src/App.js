0ba255b28717f8cd2730b1fe4f11b40b60fa80ec
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-23 00:24:07 +0600

    removed default font from index.css due to overriding issue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import { Route, Switch } from "react-router-dom"
 
 import { HomePage } from "./pages/homepage/homepage.component"
-import { Header } from "./components/header/header.component"
+import Header from "./components/header/header.component"
 import ShopPage from "./pages/shop/shop.component"
 import { SignInAndOut } from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 
@@ -48,7 +48,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { connect } from "react-redux"
 import "./header.styles.scss"
 
 import { auth } from "../../firebase/firebase.utils"
 
 import { ReactComponent as Logo } from "../../assets/crown.svg"
 
-export const Header = ({ currentUser }) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -30,3 +31,9 @@ export const Header = ({ currentUser }) => (
     </div>
   </div>
 )
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header)
