6f8aea3894698b188adf48c5d33714ecfd4b1764
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-23 01:09:40 +0600

    Added dispatch to App.js for user

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React from "react"
 import "./App.css"
 import { Route, Switch } from "react-router-dom"
+import { connect } from "react-redux"
 
 import { HomePage } from "./pages/homepage/homepage.component"
 import Header from "./components/header/header.component"
 import ShopPage from "./pages/shop/shop.component"
 import { SignInAndOut } from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
-
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 import { init_google_analytics, log_page_view } from "./utils/_analytics"
+import { setCurrentUser } from "./redux/user/user.actions"
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
-            }
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data
           })
         })
       } else {
-        this.setState({ currentUser: userAuth })
+        setCurrentUser(userAuth)
       }
     })
     init_google_analytics()
@@ -59,4 +53,8 @@ class App extends React.Component {
   }
 }
 
-export default App
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App)
