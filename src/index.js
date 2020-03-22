a74e29e5b5b05052fda423d46fd079f9e08903a9
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-22 22:33:00 +0600

    Finished writing 1st userReducer and action for the app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 
+import App from "./App"
+import "./index.css"
+import store from "./redux/store"
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-
   document.getElementById("root")
 )
