70054ce652ee389670264e22044692726662453d
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-24 22:29:54 +0600

    Working Collection

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,24 +4,28 @@ import { CustomButton } from "../custom-button/custom-button.component"
 import { connect } from "react-redux"
 import { addItem } from "../../redux/cart/cart.actions"
 
-const CollectionItem = ({ id, price, name, imageUrl, addItem }) => (
-  <div key={id} className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
+const CollectionItem = ({ item, addItem }) => {
+  const { id, price, name, imageUrl } = item
 
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+  return (
+    <div key={id} className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        ADD TO CART
+      </CustomButton>
     </div>
-    <CustomButton inverted onClick={addItem}>
-      ADD TO CART
-    </CustomButton>
-  </div>
-)
+  )
+}
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
