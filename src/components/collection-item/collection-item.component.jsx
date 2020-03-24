52b3f3960329f26de113184db404423641b94489
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-24 12:44:25 +0600

    Started cart add item redux + ADDED NETLIFY GOOGLEAPI PROXIES

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import "./collection-item.styles.scss"
 import { CustomButton } from "../custom-button/custom-button.component"
+import { connect } from "react-redux"
+import { addItem } from "../../redux/cart/cart.actions"
 
-export const CollectionItem = ({ id, price, name, imageUrl }) => (
+const CollectionItem = ({ id, price, name, imageUrl, addItem }) => (
   <div key={id} className="collection-item">
     <div
       className="image"
@@ -15,6 +17,14 @@ export const CollectionItem = ({ id, price, name, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
-    <CustomButton inverted>ADD TO CART</CustomButton>
+    <CustomButton inverted onClick={addItem}>
+      ADD TO CART
+    </CustomButton>
   </div>
 )
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,24 +1,17 @@
-import React from 'react'
-import './collection-preview.styles.scss'
-import { CollectionItem } from '../collection-item/collection-item.component'
+import React from "react"
+import "./collection-preview.styles.scss"
+import CollectionItem from "../collection-item/collection-item.component"
 
 export const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
 
-    <div className="collection-preview">
-
-        <h1 className="title">{title.toUpperCase()}</h1>
-
-        <div className="preview">
-
-            {
-                items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
-                    <CollectionItem 
-                        key = {id}
-                        {...otherItemProps}
-                    />
-                ))
-            }
-
-        </div>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
+  </div>
 )
