6c8331111f77392566e7f4b32ed7a97522fe8ab1
Zoha <ahamed.zoha@gmail.com>
2020-04-22 17:01:11 +0600

    Added redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware } from "redux"
 import logger from "redux-logger"
 import { persistStore } from "redux-persist"
+import { composeWithDevTools } from "redux-devtools-extension"
 
 import rootReducer from "./root-reducer"
 
 const middlewares = [logger]
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
 
 export const persistor = persistStore(store)
 
